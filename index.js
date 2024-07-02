e2d6935b2c229a0c06246ff37a0165906b7e0ebd
Starlyn_Fernandez <Starlyn_Fernandez@wws.com.do>
2024-07-02 11:32:39 -0400

    add enhacements in server

--- index.js
+++ index.js
@@ -16,9 +16,31 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const appInsights = require('applicationinsights');
+appInsights.setup("77dd4ef2-a631-4c76-99c0-1bbed8869c2a")    .setAutoCollectRequests(true)
+    .setAutoDependencyCorrelation(true)
+    .setAutoCollectRequests(true)
+    .setAutoCollectPerformance(true, true)
+    .setAutoCollectExceptions(true)
+    .setAutoCollectDependencies(true)
+    .setAutoCollectConsole(true)
+    .setUseDiskRetryCaching(true)
+    .setSendLiveMetrics(false)
+    .setDistributedTracingMode(appInsights.DistributedTracingModes.AI)
+    .start();
+
+// Import the useAzureMonitor function and the AzureMonitorOpenTelemetryOptions class from the @azure/monitor-opentelemetry package.
+const { useAzureMonitor, AzureMonitorOpenTelemetryOptions } = require("@azure/monitor-opentelemetry");
+
+// Create a new AzureMonitorOpenTelemetryOptions object.
+const options = {
+  azureMonitorExporterOptions: {
+    connectionString: "InstrumentationKey=77dd4ef2-a631-4c76-99c0-1bbed8869c2a;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/;ApplicationId=6df8d8e6-9fbf-49e9-95ea-1990a0aa3747"
+  }
+};
+
+// Enable Azure Monitor integration using the useAzureMonitor function and the AzureMonitorOpenTelemetryOptions object.
+useAzureMonitor(options);
 
 // route for auth
 app.use("/api/auth", authRoutes);
